9a4ae14516ea8394edb42dc415c8e6450e9a5143
hyo814 <ggamjige8888@naver.com>
2024-06-17 14:02:12 +0900

    글쓰기 추가

---
+++ app/new-post/page.tsx
@@ -0,0 +1,9 @@
+import NewPostForm from '@/components/NewPostForm'
+export default function NewPostPage() {
+  return (
+    <>
+      <h1>Create a New Post</h1>
+      <NewPostForm />
+    </>
+  )
+}
