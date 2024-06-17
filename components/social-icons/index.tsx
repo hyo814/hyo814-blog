6cd2dc2548f0c79fda5924c3cda413dccbd05177
hyo814 <ggamjige8888@naver.com>
2024-06-17 10:34:14 +0900

    시도 해보자~~

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -1,27 +1,13 @@
 import {
   Mail,
   Github,
-  Facebook,
-  Youtube,
   Linkedin,
-  Twitter,
-  X,
-  Mastodon,
-  Threads,
-  Instagram,
 } from './icons'
 
 const components = {
   mail: Mail,
   github: Github,
-  facebook: Facebook,
-  youtube: Youtube,
   linkedin: Linkedin,
-  twitter: Twitter,
-  x: X,
-  mastodon: Mastodon,
-  threads: Threads,
-  instagram: Instagram,
 }
 
 type SocialIconProps = {
