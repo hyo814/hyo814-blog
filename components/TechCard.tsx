6ee0e0a09078f9374c5948ec427e0a8be721b894
hyo814 <ggamjige8888@naver.com>
2024-09-30 19:51:10 +0900

    svg 파일 적용

--- components/TechCard.tsx
+++ components/TechCard.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
-import Image from 'next/image'
+import Image from 'next/image';
+import React, { useState } from 'react';
 
 interface TechCardProps {
-  frontContent: string
-  backContent: string
-  imageSrc: string
+  frontContent: string;
+  backContent: string;
+  imageSrc: string;
 }
 
 const TechCard: React.FC<TechCardProps> = ({ frontContent, backContent, imageSrc }) => {
-  const [flipped, setFlipped] = useState(false)
-
+  const [flipped, setFlipped] = useState(false);
+  
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
     if (event.key === 'Enter' || event.key === ' ') {
-      setFlipped(!flipped)
+      setFlipped(!flipped);
     }
-  }
-
+  };
+  
   return (
     <div
       className="flex h-72 w-52 cursor-pointer items-center justify-center md:h-80 md:w-64"
@@ -33,7 +33,7 @@ const TechCard: React.FC<TechCardProps> = ({ frontContent, backContent, imageSrc
           className="absolute flex h-full w-full flex-col items-center justify-center rounded-lg bg-blue-500 p-4 shadow-lg"
           style={{ backfaceVisibility: 'hidden', zIndex: flipped ? 0 : 1 }}
         >
-          <Image src={imageSrc} alt={frontContent} className="mb-4 h-16 w-16" />
+          <Image src={imageSrc} alt={frontContent} width={300} height={300}/>
           <h2 className="break-words text-center text-xl text-white">{frontContent}</h2>
         </div>
         <div
@@ -44,7 +44,7 @@ const TechCard: React.FC<TechCardProps> = ({ frontContent, backContent, imageSrc
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TechCard
+export default TechCard;
