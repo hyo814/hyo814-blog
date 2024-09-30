210dd2548f97409e02998ba732732d829233cdba
hyo814 <ggamjige8888@naver.com>
2024-09-30 16:57:00 +0900

    프리티어 수정

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,37 +1,36 @@
 import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
-import {genPageMetadata} from 'app/seo'
+import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({title: 'Projects'})
+export const metadata = genPageMetadata({ title: 'Projects' })
 
 export default function Projects() {
-	return (
-		<>
-			<div className="divide-y divide-gray-200 dark:divide-gray-700">
-				<div className="space-y-2 pb-8 pt-6 md:space-y-5">
-					<h1
-						className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-						Projects
-					</h1>
-					<p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-						프로젝트 각 이미지를 클릭하시면 <br/>
+  return (
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Projects
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            프로젝트 각 이미지를 클릭하시면 <br />
             주요 내용, 역할, 기간등이 들어있는 세부 링크를 통해 확인하실 수 있습니다.
-					</p>
-				</div>
-				<div className="container py-12">
-					<div className="-m-4 flex flex-wrap">
-						{projectsData.map((d) => (
-							<Card
-								key={d.title}
-								title={d.title}
-								description={d.description}
-								imgSrc={d.imgSrc}
-								href={d.href}
-							/>
-						))}
-					</div>
-				</div>
-			</div>
-		</>
-	)
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
--- components/TechCard.tsx
+++ components/TechCard.tsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 interface TechCardProps {
-	frontContent: string;
-	backContent: string;
-	imageSrc: string;
+  frontContent: string
+  backContent: string
+  imageSrc: string
 }
 
 const TechCard: React.FC<TechCardProps> = ({ frontContent, backContent, imageSrc }) => {
-	const [flipped, setFlipped] = useState(false);
-	
-	return (
-		<div
-			className="w-52 h-72 md:w-64 md:h-80 flex items-center justify-center cursor-pointer"
-			onClick={() => setFlipped(!flipped)}
-			style={{ perspective: '1000px' }}
-		>
-			<div
-				className={`relative w-full h-full transition-transform duration-500 ${flipped ? 'transform rotate-y-180' : ''}`}
-				style={{ transformStyle: 'preserve-3d' }}
-			>
-				<div
-					className="absolute w-full h-full bg-blue-500 flex flex-col items-center justify-center rounded-lg shadow-lg p-4"
-					style={{ backfaceVisibility: 'hidden', zIndex: flipped ? 0 : 1 }}
-				>
-					<img src={imageSrc} alt={frontContent} className="w-16 h-16 mb-4" /> {/* 이미지 배지 */}
-					<h2 className="text-white text-xl text-center break-words">{frontContent}</h2>
-				</div>
-				<div
-					className="absolute w-full h-full bg-green-500 flex items-center justify-center rounded-lg shadow-lg transform rotate-y-180"
-					style={{ backfaceVisibility: 'hidden', zIndex: flipped ? 1 : 0 }}
-				>
-					<p className="text-white text-xl">{backContent}</p>
-				</div>
-			</div>
-		</div>
-	);
-};
+  const [flipped, setFlipped] = useState(false)
 
-export default TechCard;
+  return (
+    <div
+      className="flex h-72 w-52 cursor-pointer items-center justify-center md:h-80 md:w-64"
+      onClick={() => setFlipped(!flipped)}
+      style={{ perspective: '1000px' }}
+    >
+      <div
+        className={`relative h-full w-full transition-transform duration-500 ${flipped ? 'rotate-y-180 transform' : ''}`}
+        style={{ transformStyle: 'preserve-3d' }}
+      >
+        <div
+          className="absolute flex h-full w-full flex-col items-center justify-center rounded-lg bg-blue-500 p-4 shadow-lg"
+          style={{ backfaceVisibility: 'hidden', zIndex: flipped ? 0 : 1 }}
+        >
+          <img src={imageSrc} alt={frontContent} className="mb-4 h-16 w-16" /> {/* 이미지 배지 */}
+          <h2 className="break-words text-center text-xl text-white">{frontContent}</h2>
+        </div>
+        <div
+          className="rotate-y-180 absolute flex h-full w-full transform items-center justify-center rounded-lg bg-green-500 shadow-lg"
+          style={{ backfaceVisibility: 'hidden', zIndex: flipped ? 1 : 0 }}
+        >
+          <p className="text-xl text-white">{backContent}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TechCard
