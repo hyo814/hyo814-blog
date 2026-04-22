76e10638c84a724bd20ffcdbb10629375a577515
imhyojin <hjlim@gitsn.com>
2026-04-22 15:00:00 +0900

    remove: Skills 페이지 제거 및 About 페이지로 내용 흡수

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/projects', title: 'Projects' },
-  { href: '/skills', title: 'Skills' },
   { href: '/timeline', title: 'TimeLine' },
   { href: '/tags', title: 'Tags' },
   { href: '/blog', title: 'Blog' },
